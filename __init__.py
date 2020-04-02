c01d99f432551298f60828f416f912f3d17960e5
Michael McDermott <mcdermott.michaelj@gmail.com>
2020-04-01 21:24:42 -0500

    First draft of the plugin.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from calibre.customize.conversion import OutputFormatPlugin
 from calibre.ptempfile import PersistentTemporaryDirectory
+from calibre import CurrentDir
 import os, re, shutil
 
 class ComicOutputPlugin(OutputFormatPlugin):
@@ -15,7 +16,33 @@ class ComicOutputPlugin(OutputFormatPlugin):
     def convert(self, oeb_book, output_path, input_plugin, opts, log):
         from calibre.utils import zipfile
         from templite import Templite
+        from lxml import etree
+
+        image_types = ['image/jpeg', 'image/png']
 
         tempdir = os.path.realpath(PersistentTemporaryDirectory())
 
         log.info('Creating temp dir ' + tempdir)
+
+        page_no = 0
+
+        with CurrentDir(tempdir):
+            for item in oeb_book.manifest:
+                if item.media_type in image_types:
+                    log.info('Found image ' + item.id + ' ' + item.media_type + ' ' + item.href)
+
+                    file_name = os.path.join(tempdir, os.path.basename(item.href))
+                    with open(file_name, 'wb') as image:
+                        image.write(item.data)
+
+        log.info('Finished extracting images, repackaging them as CBZ ' + output_path)
+
+        zfile = zipfile.ZipFile(output_path, mode="w")
+        zfile.add_dir(tempdir, '/')
+        log.info('Added files. Preparing to compress.')
+
+        zfile.write(output_path, os.path.basename(output_path), zipfile.ZIP_DEFLATED)
+
+        log.info('Cleaning up temp dir...')
+        shutil.rmtree(tempdir)
+        log.info('All done.')
